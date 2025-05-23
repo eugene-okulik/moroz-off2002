b3222a94c453eaaa7cf44e99277804b0bc00f32e
Alexander Morozov <moroz-off2002@mail.ru>
2025-05-23 13:05:10 +0300

    fixing lint errors 3

--- homework/aleksandr_morozov/homework_7/task2.py
+++ homework/aleksandr_morozov/homework_7/task2.py
@@ -4,5 +4,5 @@
 words = {'I': 3, 'love': 5, 'Python': 1, '!': 50}
 
 for key, value in words.items():
-        print(key * value, end='')
-        print()
+    print(key * value, end='')
+    print()
