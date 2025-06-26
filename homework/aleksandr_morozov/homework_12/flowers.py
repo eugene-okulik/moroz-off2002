d19ed400ba12d1787e8391e7225fa050ea6e2265
Alexander Morozov <moroz-off2002@mail.ru>
2025-06-26 15:37:16 +0300

    added parameters into parent class, combined sorting method with getattr

--- homework/aleksandr_morozov/homework_12/flowers.py
+++ homework/aleksandr_morozov/homework_12/flowers.py
@@ -2,10 +2,21 @@
 
 
 class Flowers:
+    lifetime = 1
+    stemlength = 1
+    price = 1
 
     def __init__(self, freshness, colour):
         self.freshness = freshness
         self.colour = colour
+
+    def __str__(self):
+        return (f'\n{self.__class__.__name__}, свежесть: {self.freshness}, цвет: {self.colour}, цена: {self.price}, '
+                f'длина стебля: {self.stemlength}, время жизни: {self.lifetime}')
+
+    def __repr__(self):
+        return (f'\n{self.__class__.__name__}, свежесть: {self.freshness}, цвет: {self.colour}, цена: {self.price}, '
+                f'длина стебля: {self.stemlength}, время жизни: {self.lifetime}')
 
 
 class Rose(Flowers):
@@ -13,41 +24,17 @@ class Rose(Flowers):
     stemlength = 50
     price = 200
 
-    def __str__(self):
-        return (f'\n{self.__class__.__name__}, свежесть: {self.freshness}, цвет: {self.colour}, цена: {self.price}, '
-                f'длина стебля: {self.stemlength}, время жизни: {self.lifetime}')
-
-    def __repr__(self):
-        return (f'\n{self.__class__.__name__}, свежесть: {self.freshness}, цвет: {self.colour}, цена: {self.price}, '
-                f'длина стебля: {self.stemlength}, время жизни: {self.lifetime}')
-
 
 class Tulip(Flowers):
     lifetime = 5
     stemlength = 30
     price = 100
 
-    def __str__(self):
-        return (f'\n{self.__class__.__name__}, свежесть: {self.freshness}, цвет: {self.colour}, цена: {self.price}, '
-                f'длина стебля: {self.stemlength}, время жизни: {self.lifetime}')
-
-    def __repr__(self):
-        return (f'\n{self.__class__.__name__}, свежесть: {self.freshness}, цвет: {self.colour}, цена: {self.price}, '
-                f'длина стебля: {self.stemlength}, время жизни: {self.lifetime}')
-
 
 class Chamomile(Flowers):
     lifetime = 7
     stemlength = 20
     price = 50
-
-    def __str__(self):
-        return (f'\n{self.__class__.__name__}, свежесть: {self.freshness}, цвет: {self.colour}, цена: {self.price}, '
-                f'длина стебля: {self.stemlength}, время жизни: {self.lifetime}')
-
-    def __repr__(self):
-        return (f'\n{self.__class__.__name__}, свежесть: {self.freshness}, цвет: {self.colour}, цена: {self.price}, '
-                f'длина стебля: {self.stemlength}, время жизни: {self.lifetime}')
 
 
 class Bouquet:
@@ -60,20 +47,8 @@ class Bouquet:
         avg_lifetime = sum_lifetime / len(self.list)
         return avg_lifetime
 
-    def sort_by_colour(self):
-        sorted_list = sorted(self.list, key=lambda x: x.colour)
-        return sorted_list
-
-    def sort_by_freshness(self):
-        sorted_list = sorted(self.list, key=lambda x: x.freshness)
-        return sorted_list
-
-    def sort_by_price(self):
-        sorted_list = sorted(self.list, key=lambda x: x.price)
-        return sorted_list
-
-    def sort_by_stemlength(self):
-        sorted_list = sorted(self.list, key=lambda x: x.stemlength)
+    def sort_by(self, key):
+        sorted_list = sorted(self.list, key=lambda x: getattr(x, key))
         return sorted_list
 
     def filter_by_lifetime(self, lifetime):
@@ -90,12 +65,13 @@ flower2 = Tulip('normal', 'yellow')
 flower3 = Chamomile('very good', 'white')
 flower4 = Rose('good', "white")
 flower5 = Tulip('bad', "violet")
+flower6 = Flowers('green', 'excelent')
 
-bouquet1 = Bouquet(flower1, flower2, flower5, flower4, flower3)
+bouquet1 = Bouquet(flower1, flower2, flower6, flower5, flower4, flower3)
 print(bouquet1.avg_lifetime())
 print(bouquet1.list)
-print(bouquet1.sort_by_colour())
-print(bouquet1.sort_by_freshness())
-print(bouquet1.sort_by_price())
-print(bouquet1.sort_by_stemlength())
-print(bouquet1.filter_by_lifetime(5))
+print(bouquet1.sort_by('colour'))
+print(bouquet1.sort_by('freshness'))
+print(bouquet1.sort_by('price'))
+print(bouquet1.sort_by('stemlength'))
+print(bouquet1.filter_by_lifetime(1))
