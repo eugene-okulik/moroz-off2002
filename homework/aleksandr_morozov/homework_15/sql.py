c697df986a9b02ee98b75883d58a41a33f6ec2e3
Alexander Morozov <moroz-off2002@mail.ru>
2025-07-04 18:30:46 +0300

    homework_15_part2

--- homework/aleksandr_morozov/homework_15/sql.py
+++ homework/aleksandr_morozov/homework_15/sql.py
@@ -2,6 +2,7 @@
 
 import mysql.connector as mysql
 
+
 # задаем параметры подключения в БД
 db = mysql.connect(
     user='st-onl',
@@ -33,10 +34,24 @@ cursor.execute("INSERT INTO `groups` (title, start_date, end_date) VALUES ('Crea
                "'August 2025'")
 group_id = cursor.lastrowid   # сохраняем в переменную id созданной группы
 
+# и определите своего студента туда
+cursor.execute(f"UPDATE students SET group_id = {group_id} WHERE id = {student_id}")
 
-data = cursor.fetchall()
-print(data)
-print(cursor.fetchone())
+# Создайте несколько учебных предметов (subjects), создайте по два занятия для каждого предмета (lessons)
+subjects = ['Архитектура данных', 'Системная аналитика']
+lessons = ['1. Актуальность', '2. Основные термины']
+lessons_id = []
+for subject in subjects:
+    cursor.execute(f"INSERT INTO subjects (title) VALUES ('{subject}')")
+    subject_id = cursor.lastrowid
+    for lesson in lessons:
+        cursor.execute(f"INSERT INTO lessons (title, subject_id) VALUES ('{lesson}', '{subject_id}')")
+        lessons_id.append(cursor.lastrowid)
+print(lessons_id)
+
+marks = ['good', 'excellent', 'passed', 'failed']
+for lesson in lessons_id:
+    cursor.execute(f"INSERT INTO marks (value, lesson_id, student_id) VALUES ('{mark}', '{lesson}', {student_id})")
 
 db.commit()
 
