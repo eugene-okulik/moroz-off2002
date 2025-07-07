e6161ea1028e6e99879ca202f728cbce5391956e
Alexander Morozov <moroz-off2002@mail.ru>
2025-07-07 13:25:50 +0300

    homework_15_finished

--- homework/aleksandr_morozov/homework_15/task_mysql.py
+++ homework/aleksandr_morozov/homework_15/task_mysql.py
@@ -1,7 +1,11 @@
 # Работа с БД MySQL через Python
 
 import mysql.connector as mysql
+import random
 
+def print_cursor(func):
+    for row in func:
+        print(row)
 
 # задаем параметры подключения в БД
 db = mysql.connect(
@@ -16,8 +20,9 @@ db = mysql.connect(
 cursor = db.cursor(dictionary=True)
 
 # Создайте студента (student)
-insert_student_query = "INSERT INTO students (name, second_name, group_id) VALUES (%s, %s, NULL);"
-cursor.execute(insert_student_query, (input('name: '), input('second_name: ')))
+insert_student_query = "INSERT INTO students (name, second_name, group_id) VALUES (%s, %s, NULL)"
+cursor.execute(insert_student_query, (input('Имя '), (input('Фамилия '))))
+# print(cursor.statement)
 student_id = cursor.lastrowid   # сохраняем в переменную id созданного студента
 
 # Создайте несколько книг (books) и укажите, что ваш созданный студент взял их
@@ -30,8 +35,10 @@ cursor.executemany(
 )
 
 # Создайте группу (group)
-cursor.execute("INSERT INTO `groups` (title, start_date, end_date) VALUES ('Created by Python', 'April 2025', "
-               "'August 2025'")
+cursor.execute(
+    "INSERT INTO `groups` (title, start_date, end_date) VALUES ('Created by Python', 'April 2025', 'August 2025')"
+)
+# print(cursor.statement)
 group_id = cursor.lastrowid   # сохраняем в переменную id созданной группы
 
 # и определите своего студента туда
@@ -41,18 +48,45 @@ cursor.execute(f"UPDATE students SET group_id = {group_id} WHERE id = {student_i
 subjects = ['Архитектура данных', 'Системная аналитика']
 lessons = ['1. Актуальность', '2. Основные термины']
 lessons_id = []
+marks_id = []
+
 for subject in subjects:
-    cursor.execute(f"INSERT INTO subjects (title) VALUES ('{subject}')")
+    cursor.execute(f"INSERT INTO subjets (title) VALUES ('{subject}')")
     subject_id = cursor.lastrowid
     for lesson in lessons:
         cursor.execute(f"INSERT INTO lessons (title, subject_id) VALUES ('{lesson}', '{subject_id}')")
         lessons_id.append(cursor.lastrowid)
-print(lessons_id)
+        mark = random.randint(61, 100)
+        cursor.execute(f"INSERT INTO marks (value, lesson_id, student_id) VALUES ('{mark}', '{cursor.lastrowid}', {student_id})")
+        marks_id.append(cursor.lastrowid)
 
-marks = ['good', 'excellent', 'passed', 'failed']
-for lesson in lessons_id:
-    cursor.execute(f"INSERT INTO marks (value, lesson_id, student_id) VALUES ('{mark}', '{lesson}', {student_id})")
-
+print('student_id', student_id, 'group_id', group_id)
+print('id добавленных уроков: ', lessons_id, 'id добавленных оценок: ', marks_id)
 db.commit()
+
+cursor.execute(f"SELECT * FROM marks WHERE student_id = {student_id}")
+result_marks = list(cursor.fetchall())
+
+cursor.execute(f"SELECT * FROM books WHERE taken_by_student_id = {student_id}")
+result_books = list(cursor.fetchall())
+
+select_query = f'''
+SELECT s.name as 'Имя', s.second_name as 'Фамилия', g.title as 'Группа', s2.title as 'Предмет', m.value as 'Оценка', 
+l.title as 'Урок', b.title as 'Книга'
+FROM students s 
+JOIN `groups` g ON s.group_id = g.id
+JOIN books b ON s.id = b.taken_by_student_id
+JOIN marks m ON s.id = m.student_id
+JOIN lessons l ON m.lesson_id = l.id
+JOIN subjets s2 ON l.subject_id = s2.id
+WHERE s.id = {student_id}
+'''
+
+cursor.execute(select_query)
+result_all = list(cursor.fetchall())
+
+print_cursor(result_marks)
+print_cursor(result_books)
+print_cursor(result_all)
 
 db.close()
